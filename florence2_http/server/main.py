24ef20397ef44bb59af2aaa67ca1fb9e9765a063
Antonin Couturier <acouturier.inscriptions@gmail.com>
2024-10-05 12:50:25 -0400

    Add main entrypoint for server

---
+++ florence2_http/server/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from florence2_http.server.models import Florence2
+from florence2_http.server.schemas import TaskRequest, TaskResponse
+from florence2_http.shared import FlorenceModel
+
+app = FastAPI()
+
+model_type = FlorenceModel.BASE 
+model = Florence2(model_type)
+
+@app.post("/run_task", response_model=TaskResponse)
+async def run_task(request: TaskRequest):
+    result = model.run_task(
+        task=request.task,
+        image_base64=request.image_base64,
+        text_input=request.text_input
+    )
+    return TaskResponse(result=result)
