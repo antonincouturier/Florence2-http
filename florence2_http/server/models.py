40c3cb8b18df7d3f739b3b66c3700f51727f079f
Antonin Couturier <acouturier.inscriptions@gmail.com>
2024-10-05 11:27:21 -0400

    run_task now takes image_base64 as input rather than Image

--- florence2_http/server/models.py
+++ florence2_http/server/models.py
@@ -1,44 +1,29 @@
 # Using code from https://colab.research.google.com/#scrollTo=43333b69-5484-4c16-b3cf-331d74c36780&fileId=https%3A//huggingface.co/microsoft/Florence-2-large/blob/main/sample_inference.ipynb
 
+from enum import Enum 
+from typing import Optional, Dict
+from io import BytesIO
+import base64
+
 from transformers import AutoProcessor, AutoModelForCausalLM
 import torch
 from PIL import Image
-from enum import Enum 
-from typing import Optional, Dict
 
-class FlorenceModels(Enum):
-    BASE = "microsoft/Florence-2-base" 
-    LARGE = "microsoft/Florence-2-large"
-    BASE_FT = "microsoft/Florence-2-base-ft"
-    LARGE_FT = "microsoft/Florence-2-large-ft"
-
-class FlorenceTask(Enum):
-    CAPTION = "<CAPTION>"
-    DETAILED_CAPTION = "<DETAILED_CAPTION>"
-    MORE_DETAILED_CAPTION = "<MORE_DETAILED_CAPTION>"
-    OBJECT_DETECTION = "<OD>"
-    DENSE_REGION_CAPTION = "<DENSE_REGION_CAPTION>"
-    REGION_PROPOSAL = "<REGION_PROPOSAL>"
-    CAPTION_TO_PHRASE_GROUNDING = "<CAPTION_TO_PHRASE_GROUNDING>"
-    REFERRING_EXPRESSION_SEGMENTATION = "<REFERRING_EXPRESSION_SEGMENTATION>"
-    REGION_TO_SEGMENTATION = "<REGION_TO_SEGMENTATION>"
-    OPEN_VOCABULARY_DETECTION = "<OPEN_VOCABULARY_DETECTION>"
-    REGION_TO_CATEGORY = "<REGION_TO_CATEGORY>"
-    REGION_TO_DESCRIPTION = "<REGION_TO_DESCRIPTION>"
-    OCR = "<OCR>"
-    OCR_WITH_REGION = "<OCR_WITH_REGION>"
+from florence2_http.shared.enums import FlorenceModel, FlorenceTask
 
 class Florence2:
-    def __init__(self, model_type: FlorenceModels):
+    def __init__(self, model_type: FlorenceModel):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         model_id = model_type.value
         self.model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, torch_dtype='auto').eval().to(self.device)
         self.processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
 
-    def run_task(self, task: FlorenceTask, image: Image, text_input: Optional[str] = None) -> Dict:
+    def run_task(self, task: FlorenceTask, image_base64: str, text_input: Optional[str] = None) -> Dict:
         prompt = FlorenceTask.value
         if text_input is not None:
             prompt = f"{prompt}{text_input}"
+        image_bytes = base64.base64decode(image_base64)
+        image = Image.opne(BytesIO(image_bytes))
         inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(self.device, torch.float16)
         generated_ids = self.model.generate(
             input_ids=inputs["input_ids"],
@@ -57,4 +42,3 @@ class Florence2:
             image_size=(image.width, image.height)
         )
         return parsed_answer
-
